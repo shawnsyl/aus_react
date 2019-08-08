16b64a1e72c4220a8afeafa94ffec369e0c59737
shawnsyl <shawnseyoung.lee@mail.utoronto.ca>
2019-08-08 00:41:54 -0400

    created home page

--- src/views/HomeMain.js
+++ src/views/HomeMain.js
@@ -10,7 +10,23 @@ class HomeMain extends Component {
     return (
       <div>
         <Navbar pathname={this.props.location.pathname} />
-        homepage
+        <div className="page1">
+          <img src={require("../imgs/home/hand.png")} />
+          <div className="p1container">
+            <h1>Bleed Purple</h1>
+            <h2>UBC Arts Undergraduate Society</h2>
+            <p>AUS Elections are coming up!</p>
+            <p>Nomination period ends on Friday March 8th, at 5:00 PM.</p>
+
+            <div id="learn_more">
+              <span>Learn More</span>
+            </div>
+          </div>
+          <div className="p2container">
+              <h1>News and Events</h1>
+              
+          </div>
+        </div>
       </div>
     );
   }
